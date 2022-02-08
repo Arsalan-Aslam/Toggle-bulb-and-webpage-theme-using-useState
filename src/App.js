bf6a074638b1c2a94efe70741a2be21294c3f4db
Arsalan-Aslam <arsalanaslam89@gmail.com>
2022-02-08 11:25:49 +0500

    added buld on/off component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Screen from "./components/Screen.jsx";
+import Image from "./components/Image.jsx";
 
 function App() {
   return (
     <>
       <Screen />
+      <Image />
     </>
   );
 }
